5a8b616cc72e79d0a84a9b1cc4bfe29a4295832f
p-chiri <kipchirchir.arnold.mutai@gmail.com>
2022-01-16 16:25:38 +0300

    cimmit 2

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,6 @@
 class MainController < ApplicationController
     def index
+        flash.now[:notice ] ="logged in successfullly"
+        flash.now[:alert] = "Invalid email or password"
     end
 end
